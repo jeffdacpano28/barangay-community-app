f3314689d5c05f162916ad60cb03b3a3bc25ac53
jeffdacpano28 <dacpanojefferson@gmail.com>
2025-05-19 17:09:13 +0800

    Add AnnouncementsPage

---
+++ src/AnnouncementsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AnnouncementsPage() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">📢 Announcements</h2>
+      <p>No announcements available at this time.</p>
+    </div>
+  );
+}
+
+export default AnnouncementsPage;
